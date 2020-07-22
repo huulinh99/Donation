e0702e79bf4b38b181b01338630e7baaf232f2ca
huulinh99 <43189610+huulinh99@users.noreply.github.com>
2020-07-22 16:27:15 +0700

    Revert "Phu branch"

---
+++ SWD/Models/DonateDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWD.Models
+{
+    public partial class DonateDetail
+    {
+        public int Id { get; set; }
+        public int? CampaignId { get; set; }
+        public int? UserId { get; set; }
+        public double? Amount { get; set; }
+        public DateTime? Date { get; set; }
+        public string Description { get; set; }
+        public int? GiftId { get; set; }
+
+        public Campaign Campaign { get; set; }
+        public GiftDetail Gift { get; set; }
+        public User User { get; set; }
+    }
+}
